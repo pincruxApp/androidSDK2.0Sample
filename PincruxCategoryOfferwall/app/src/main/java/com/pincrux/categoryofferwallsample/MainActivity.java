1ec20217f128260daf102c6078f80c1bdbf2347e
mason kim <46478557+pincruxApp@users.noreply.github.com>
2025-02-07 16:49:54 +0900

    카테고리형 충전소 샘플 추가

---
+++ PincruxCategoryOfferwall/app/src/main/java/com/pincrux/categoryofferwallsample/MainActivity.java
@@ -0,0 +1,93 @@
+package com.pincrux.categoryofferwallsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.pincrux.offerwall.PincruxOfferwall;
+import com.pincrux.offerwall.util.point.impl.PincruxAdPointImpl;
+import com.pincrux.offerwall.util.point.model.PincruxAdPointInfo;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    PincruxOfferwall offerwall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        String YOUR_PUBKEY = "911871";
+
+        offerwall = PincruxOfferwall.getInstance();
+        offerwall.init(this, YOUR_PUBKEY, "pincrux_test");
+        offerwall.setOfferwallType(PincruxOfferwall.BAR_PREMIUM_TYPE);
+        offerwall.setDarkMode(PincruxOfferwall.LIGHT);
+        offerwall.setAdDetail(true);
+        offerwall.setOrientationLandscape(false);
+        offerwall.setEnableTab(true);
+        offerwall.setEnableScrollTopButton(true);
+
+        offerwall.getAdPoint(this, YOUR_PUBKEY, new PincruxAdPointImpl() {
+            @Override
+            public void onReceivePoint(PincruxAdPointInfo pincruxAdPointInfo) {
+                if (pincruxAdPointInfo != null) {
+                    String buttonString = getString(R.string.button1) + "\n" + pincruxAdPointInfo.getFinancePoint() + "P";
+                    button1.setText(buttonString);
+
+                    buttonString = getString(R.string.button2) + "\n" + pincruxAdPointInfo.getCpaPoint() + "P";
+                    button2.setText(buttonString);
+
+                    buttonString = getString(R.string.button3) + "\n" + pincruxAdPointInfo.getSocialPoint() + "P";
+                    button3.setText(buttonString);
+
+                    buttonString = getString(R.string.button4) + "\n" + pincruxAdPointInfo.getCpsPoint() + "P";
+                    button4.setText(buttonString);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1: {    // 금융적립
+                offerwall.setOfferwallCategory(1);
+                offerwall.startPincruxOfferwallActivity(MainActivity.this);
+                break;
+            }
+
+            case R.id.button2: {    // 참여적립
+                offerwall.setOfferwallCategory(2);
+                offerwall.startPincruxOfferwallActivity(MainActivity.this);
+                break;
+            }
+
+            case R.id.button3: {    // 소셜적립
+                offerwall.setOfferwallCategory(3);
+                offerwall.startPincruxOfferwallActivity(MainActivity.this);
+                break;
+            }
+
+            case R.id.button4: {    // 구매적립
+                offerwall.setOfferwallCategory(4);
+                offerwall.startPincruxOfferwallActivity(MainActivity.this);
+                break;
+            }
+        }
+    }
+}
